0674fa45d47157654e609b4572f542d3c8f3e536
rahul.wadhwa <rahul.wadhwa@techaheadcorp.com>
2022-03-03 17:23:30 +0530

    Package name changed

--- ios/ReactNativeBoilerplateTests/ReactNativeBoilerplateTests.m
+++ ios/ReactNativeBoilerplateTests/ReactNativeBoilerplateTests.m
@@ -7,11 +7,11 @@
 #define TIMEOUT_SECONDS 600
 #define TEXT_TO_LOOK_FOR @"Welcome to React"
 
-@interface ReactNativeBoilerplateTests : XCTestCase
+@interface iot4netTests : XCTestCase
 
 @end
 
-@implementation ReactNativeBoilerplateTests
+@implementation iot4netTests
 
 - (BOOL)findSubviewInView:(UIView *)view matching:(BOOL(^)(UIView *view))test
 {
